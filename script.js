026cdd5688d6c420908087553c8cbc3da14f500d
Ingrid Benicio <78183082+hewwo-ingrid@users.noreply.github.com>
2022-05-01 10:09:47 -0300

    Create script.js
    Criação do Script da pagina da Ada Lovelace - projeto do curso Eu ProgrAMO turma 16 - PrograMaria.

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById("botaoEnviar").addEventListener("click",validaFormulario)
+
+function validaFormulario(){
+  if (document.getElementById("nome").value != "" && 
+  document.getElementById("email").value != "" && 
+  document.getElementById("telefone").value !=""){
+
+    alert("Prontinho! Você receberá as novidades.")
+
+  }else{
+
+    alert("Por favor, preencha os campos nome, e-mail e telefone!")
+    
+  }
+}
+
+
+
+
